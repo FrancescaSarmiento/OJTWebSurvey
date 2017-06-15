5be0d85edb2955ddb6e4c213f556b8b6ac48180c
Catherine_Villacentino <2151857@slu.edu.ph>
2017-06-15 13:27:55 +0800

    created a form for sign up

--- registration/index.php
+++ registration/index.php
@@ -14,6 +14,10 @@
 
     
 <body >
+    <nav>
+        
+    </nav>
+    
     <?php
 
         $ntu_survey = new mysqli("localhost", "root", "", "ntu_survey");
@@ -22,23 +26,71 @@
             die("Connection failed: " . $ntu_survey->connect_error);
         }
     ?>
+    <div id = "top">
+         <a href="../landing/index.php"><button type="button" class="backbtn">Back</button></a>
+    </div>
     <div class="container">
-        <div class="reg">
-            <form role="form" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-                <label><b>Name</b></label>
-                <input type="text" placeholder="Enter Name" name="name" required>
-                
-                <br>
-                <br>
-                
-                <br>
-                <br>
-                <div class="btn">
-                    <a href="../landing/index.php"><button type="button" class="cancelbtn">Cancel</button></a>
-                    <button type="submit" class="submitbtn" name="login">Sign Up</button>
-                </div>     
-            </form>
-        </div>
+        <form role="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+            <fieldset>
+                    
+                <?php
+                    if(isset($_POST["register"])) {        
+                        $empnumber = $_POST["empnumber"];
+                        $lname = $_POST["lname"];
+                        $fname = $_POST["fname"];
+                        $email = $_POST["email"];
+                        $password = $_POST["password"];
+                        $dept = $_POST["dept"];
+                        $team = $_POST["team"]; 
+                        
+                        
+                        if (!preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $email)) {
+                                echo "The email you have entered is invalid, please try again.";
+                        } else {
+                        $result = mysqli_query($ntu_survey, "SELECT * FROM user WHERE email = '$email'");
+                            if(mysqli_num_rows($result) > 0) {
+                                   echo "<div class='err'>Account already exists!</div>";                                        mysqli_free_result($result);
+                            } else {
+                                $sql = "INSERT INTO user (iduser, lastname, firstname, email, password, department, team) 
+                                                        VALUES ('$empnumber','$lname', '$fname', '$email', md5('$password'), '$dept', '$team')";
+                                if ($ntu_survey->query($sql) === TRUE) {
+                                        header("Location: ../login/index.php");
+                                } else {
+                                        echo "Error: " . $sql . "<br>" . $ntu_survey->error;
+                                }
+                            }
+                        }
+                    } 
+                ?>
+                                   
+                <label><b>ID Number</b></label>
+                <input type="text" placeholder="Enter ID Number" name="empnumber" required>
+                    
+                <label><b>Lastname</b></label>
+                <input type="text" placeholder="Enter Lastname" name="lname" required>
+                    
+                <label><b>Firstname</b></label>
+                <input type="text" placeholder="Enter Firstname" name="fname" required>
+                <br>    
+                <label><b>E-mail Address</b></label>
+                <input type="text" placeholder="Enter E-mail" name="email" required>
+                    
+                <label><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="password" required>
+                    
+                <label><b>Department</b></label>
+                <input type="text" placeholder="Enter your Department" name="dept" required>
+                    
+                <label><b>Team</b></label>
+                <input type="text" placeholder="Enter your Team" name="team" required>
+                    
+                <button type="submit" class="submitbtn" name="register" >Sign Up</button>
+                <hr>
+                <a href="../login/index.php"><button type="button" class="btn">Already have an account?</button></a>
+                    
+                    
+            </fieldset>
+        </form>
 	</div>
     
     <script src="script.js"></script>
